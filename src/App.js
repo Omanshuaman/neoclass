8f07bcac4632943c4ddf604167f55aed7079b413
Omanshu Aman <omanshuaman@gmail.com>
2023-03-07 11:53:10 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { MathJax, MathJaxContext } from "better-react-mathjax";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  useEffect(() => {
+    const questionIDs = [
+      "AreaUnderTheCurve_901",
+      "BinomialTheorem_901",
+      "DifferentialCalculus2_901",
+    ];
+
+    const fetchData = async () => {
+      const questionData = await Promise.all(
+        questionIDs.map((id) =>
+          axios.get(
+            `https://0h8nti4f08.execute-api.ap-northeast-1.amazonaws.com/getQuestionDetails/getquestiondetails?QuestionID=${id}`
+          )
+        )
+      );
+      const questions = questionData.map((data) => data.data[0].Question);
+      setQuestions(questions);
+    };
+
+    fetchData();
+  }, []);
+  const previousQuestion = () => {
+    setCurrentQuestionIndex(currentQuestionIndex - 1);
+  };
+
+  const nextQuestion = () => {
+    setCurrentQuestionIndex(currentQuestionIndex + 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MathJaxContext>
+      <div className="container">
+        <div className="button-container">
+          <button
+            className="quiz-button"
+            onClick={previousQuestion}
+            disabled={currentQuestionIndex === 0}
+          >
+            Previous Question
+          </button>
+          <button
+            className="quiz-button"
+            onClick={nextQuestion}
+            disabled={currentQuestionIndex === questions.length - 1}
+          >
+            Next Question
+          </button>
+        </div>
+        <h2 className="quiz-title">Question {currentQuestionIndex + 1}</h2>
+        <div className="quiz-question-container">
+          <MathJax>{questions[currentQuestionIndex]}</MathJax>
+        </div>
+      </div>
+    </MathJaxContext>
   );
 }
 
